f78be2a15468ecf030c59545ea520eb34b54eeba
eshaalal <eshalal888@gmail.com>
2024-01-26 11:33:35 +0530

    changes

---
+++ variable/Variable.java
@@ -0,0 +1,24 @@
+public class Variable{
+    public static void main (String[] args){
+       int num =567;
+       System.out.println(num);
+       num=23445;
+       System.out.println(num);
+
+
+       boolean isVegetarian = true;
+       System.out.println(isVegetarian);
+
+       float val= 5.0f;
+       System.out.println(val);
+
+       double val2=4.665646;
+       System.out.println(val2);
+
+       String wish="Good Morning!";
+       System.out.println(wish);
+
+
+
+    }
+}
