af2c8e66aff8d71e83f3ad08c4bbbf431c86d242
eshaalal <eshalal888@gmail.com>
2024-01-26 10:38:10 +0530

    changes

---
+++ RightHalfPyramid.java
@@ -0,0 +1,8 @@
+public class RightHalfPyramid{
+    public static void main(String args[]){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
